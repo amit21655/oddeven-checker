9a5d7f35dcda1c51c2f05d1b912aee4738c21721
Amit G <amit@sundewsolutions.com>
2026-09-16 13:27:30 +0530

    Add service provider, facade, Laravel auto-discovery

--- src/Facades/OddEven.php
+++ src/Facades/OddEven.php
@@ -1,15 +1,12 @@
 <?php
-
 namespace Anay\OddEvenChecker\Facades;
 
-use Anay\OddEvenChecker\OddEvenChecker;
 use Illuminate\Support\Facades\Facade;
 
 class OddEven extends Facade
 {
-
     protected static function getFacadeAccessor(): string
     {
-        return OddEvenChecker::class;   // resolve the real class, no string alias needed
+        return 'oddeven';
     }
 }
